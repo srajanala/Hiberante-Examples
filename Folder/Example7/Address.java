65a0d5566a6f023daf803b11f7570f30c4e42c21
Sai Sindhu <srajanala@users.noreply.github.com>
2017-12-12 15:20:18 -0600

    Add files via upload

---
+++ Folder/Example7/Address.java
@@ -0,0 +1,36 @@
+package com.hibernate;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	private String Street;
+	private String City;
+	private String State;
+	private String Pincode;
+	public String getStreet() {
+		return Street;
+	}
+	public void setStreet(String street) {
+		Street = street;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getPincode() {
+		return Pincode;
+	}
+	public void setPincode(String pincode) {
+		Pincode = pincode;
+	}
+    
+}
